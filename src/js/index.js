8af9fd70a3d283cc1e6e28fb34a568da1e7ccca0
Dorian <doriancursos@gmail.com>
2024-05-06 19:52:18 +0200

    demo 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const hamburguerIconElement = document.getElementById('hamburger-icon');
+const menuElement = document.getElementById('menu');
+
+const handleMenu = () => {
+  menuElement.classList.toggle('menu--show');
+  hamburguerIconElement.classList.toggle('hamburger-icon--open');
+};
+
+hamburguerIconElement.addEventListener('click', handleMenu);
